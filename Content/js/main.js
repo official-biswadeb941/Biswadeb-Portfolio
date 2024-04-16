7d90ef3b4f1ca84a92197474614b82be09de828a
Biswadeb Mukherjee Official <biswadebmukherjee941@gmail.com>
2024-04-17 01:28:45 +0530

    Update main.js

--- Content/js/main.js
+++ Content/js/main.js
@@ -300,5 +300,12 @@ window.addEventListener('load', function(){
     typeText();
 });
 
+	<!-- Google tag (gtag.js) -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MLM138KNXE');
 
 }());
